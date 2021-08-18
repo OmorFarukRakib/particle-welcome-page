7605eac49531f9a44dc043fbf0f697e03c7ce711
OmorFarukRakib <rakib1001@gmail.com>
2021-08-18 14:57:40 +0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Logo from "./svg";
+import { useState, useEffect } from "react";
+import ParticleBackground from "./ParticalBackground";
 function App() {
+  const [logoshow, setLogoShow] = useState(true);
+  const getlogosize = () => {
+    const logo = document.querySelectorAll("#ultra-logo path");
+    console.log(logo);
+    for (let i = 0; i < logo.length; i++) {
+      console.log(`letter ${i} -> ${logo[i].getTotalLength()}`);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLogoShow(false);
+    }, 4500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {logoshow && (
+        <>
+          <div className="logo-background">
+            <ParticleBackground />
+            <Logo />
+            <div className="logo-sub-name"> Asia Pacific </div>
+          </div>
+        </>
+      )}
+      {!logoshow && <div className="App">Main Page</div>}
+    </>
   );
 }
-
 export default App;
---
+++ src/ParticalBackground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Particles from "react-particles-js";
+import particleConfig from "./config/particle-config";
+
+const ParticleBackground = () => {
+  return <Particles params={particleConfig}></Particles>;
+};
+export default ParticleBackground;
